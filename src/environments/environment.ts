5def5ad35bd38fac0d2a415b712e0840afc874b6
Khalil Sutherland <88902077+KhalilSuth001100@users.noreply.github.com>
2023-06-04 12:45:26 -0400

    Initial Commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  weatherApiBaseUrl:"https://open-weather13.p.rapidapi.com/city/atlanta",
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'f1b102b5fdmshb43163b7faedee2p1db36bjsn8def3338ec15'
+};
